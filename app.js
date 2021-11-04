a2e0f76fadb363ebe2647455809f0f06b25fd4de
Martial Wehrling <d.wehrling@gmail.com>
2021-11-04 16:41:17 +0100

    Ajout des modales et class modal

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-////////////////////////////////////////////////////////////////
-// canvas one gamer
+////////////// canvas one gamer ////////////////////
 
 const canvas = document.getElementById('draw1');
 const ctx = canvas.getContext('2d');
@@ -158,11 +157,7 @@ penduOneGamer.set(7, rightArm);
 penduOneGamer.set(8, rightFoot);
 penduOneGamer.set(9, leftFoot);
 
-//end canvas on gamer
-//////////////////////////////////////////////////////////////////////
-
-/////////////////////////////////////////////////////////////////////
-// canvas two gamers
+////////////// canvas two gamers ///////////////////////
 
 const reset = document.getElementById('reset');
 let color = document.getElementById('color');
@@ -214,94 +209,27 @@ reset.addEventListener('click', () => {
   ctxTwo.clearRect(0, 0, canvasTwo.width, canvasTwo.height);
 });
 
-//end canvas two gamers
-//////////////////////////////////////////////////////////////////////
+///////////////////// MODALES ///////////////////////////////
 
-const title = document.querySelector('.title');
-const hideWord = document.querySelector('.hideword');
-const buttonAlphabet = document.querySelectorAll('.alphabet button');
-let count = 0;
-let letterFound = false;
-let tempWord = [];
+const modalRule = document.querySelector('.modal-rules');
+const modalParameter = document.querySelector('.modal-parameter');
+const buttonParameter = document.getElementById('begin');
+const contentGame = document.querySelector('.modal-game');
+/// AJOUTER MODALES EN FONCTION DU NOMBRE DE JOUEURS/// !!!!
 
-title.textContent = 'LE JEU DU PENDU';
-let wordHide = document.createElement('p');
-let buttonLetter = '';
-
-//base of words
-const words = [
-  'physique',
-  'voiture',
-  'argentine',
-  'forestier',
-  'voyage',
-  'australie',
-  'etats-unies',
-  'echelle',
-  'autoroute',
-  'constitution',
-  'histoire',
-  'geographie',
-];
-
-// Choice word
-let wordRandomArray = words[Math.floor(Math.random() * words.length)];
-tempWord.push(wordRandomArray.toUpperCase());
-let selectWord = wordRandomArray.toString().toUpperCase();
-
-///////////////////////////////////////
-// code new part
-if (tempWord.includes(selectWord)) {
-  for (let i = 0; i < words.length; i++) {
-    if (words[i] === wordRandomArray) {
-      words.splice(i, 1);
-    }
-  }
-  wordRandomArray = words[Math.floor(Math.random() * words.length)];
-  console.log('word random apres if ' + wordRandomArray);
-  selectWord = wordRandomArray.toString().toUpperCase();
-}
-//////////////////////////////////////
-
-let wordDisplayHide = selectWord.replace(/[A-Z]/g, '_'); //replace letter for "_" via regex
-
-//display hide word
-wordHide.textContent = wordDisplayHide;
-hideWord.appendChild(wordHide);
-
-let displayLetter = [...wordDisplayHide]; // extend hide word in array
-
-//function seek letter of word
-const seekLetter = (userLetter) => {
-  return [...selectWord].map((letter, i) => {
-    if (displayLetter[i] !== '_') return displayLetter[i];
-    if (letter === userLetter) {
-      letterFound = true;
-      return userLetter;
-    } else {
-      return '_';
-    }
-  });
+window.onload = () => {
+  modalRule.classList.add('active');
+  modalRule.classList.remove('modal-rules');
 };
 
-//event alphabet button
-buttonAlphabet.forEach((letters) => {
-  letters.addEventListener('click', (e) => {
-    buttonLetter = e.target.id.toUpperCase();
-    letters.classList.add('boxDisabled');
-    displayLetter = seekLetter(buttonLetter);
-    if (!letterFound) {
-      // if choice letter is not found - draw pendu
-      ++count;
-      penduOneGamer.get(count)();
-      letterFound = false;
-    }
-    letterFound === true ? (letterFound = false) : '';
-    wordHide.textContent = displayLetter.join(''); // display DOM
-  });
+buttonParameter.addEventListener('click', () => {
+  modalRule.classList.remove('active');
+  modalRule.classList.add('modal-rules');
+  modalParameter.classList.add('active');
+  modalParameter.classList.remove('modal-parameter');
 });
 
-//////////Choice parameters game///////////////////////////////
+////////////// Choice parameters game ///////////////////////////
 
 const buttonGamer = document.querySelectorAll('input[name="gamer"]');
 const pseudoOne = document.getElementById('p1');
@@ -421,6 +349,94 @@ valid.addEventListener('click', (e) => {
     parameter.wordNumber = wordEndGame;
     parameter.level = difficult;
     resetInputs();
-    document.location = '../html/game.html';
+    modalParameter.classList.remove('active');
+    modalParameter.classList.add('modal-parameter');
+    contentGame.classList.add('active');
+    contentGame.classList.remove('modal-game');
+    /// AJOUTER MODALES EN FONCTION DU NOMBRE DE JOUEURS !!! AVEC CONDITION PLAYERS
   }
+});
+
+//////////////////////////// GAME //////////////////////////////
+
+const hideWord = document.querySelector('.hideword');
+const buttonAlphabet = document.querySelectorAll('.alphabet button');
+let count = 0;
+let letterFound = false;
+let tempWord = [];
+
+let wordHide = document.createElement('p');
+let buttonLetter = '';
+
+//base of words
+const words = [
+  'physique',
+  'voiture',
+  'argentine',
+  'forestier',
+  'voyage',
+  'australie',
+  'etats-unies',
+  'echelle',
+  'autoroute',
+  'constitution',
+  'histoire',
+  'geographie',
+];
+
+// Choice word
+let wordRandomArray = words[Math.floor(Math.random() * words.length)];
+tempWord.push(wordRandomArray.toUpperCase());
+let selectWord = wordRandomArray.toString().toUpperCase();
+
+///////////////////////////////////////
+// code new part
+if (tempWord.includes(selectWord)) {
+  for (let i = 0; i < words.length; i++) {
+    if (words[i] === wordRandomArray) {
+      words.splice(i, 1);
+    }
+  }
+  wordRandomArray = words[Math.floor(Math.random() * words.length)];
+  console.log('word random apres if ' + wordRandomArray);
+  selectWord = wordRandomArray.toString().toUpperCase();
+}
+//////////////////////////////////////
+
+let wordDisplayHide = selectWord.replace(/[A-Z]/g, '_'); //replace letter for "_" via regex
+
+//display hide word
+wordHide.textContent = wordDisplayHide;
+hideWord.appendChild(wordHide);
+
+let displayLetter = [...wordDisplayHide]; // extend hide word in array
+
+//function seek letter of word
+const seekLetter = (userLetter) => {
+  return [...selectWord].map((letter, i) => {
+    if (displayLetter[i] !== '_') return displayLetter[i];
+    if (letter === userLetter) {
+      letterFound = true;
+      return userLetter;
+    } else {
+      return '_';
+    }
+  });
+};
+
+//event alphabet button
+buttonAlphabet.forEach((letters) => {
+  letters.addEventListener('click', (e) => {
+    buttonLetter = e.target.id.toUpperCase();
+    letters.classList.add('boxDisabled');
+    displayLetter = seekLetter(buttonLetter);
+    if (!letterFound) {
+      // if choice letter is not found - draw pendu
+      ++count;
+      penduOneGamer.get(count)();
+      letterFound = false;
+    }
+    letterFound === true ? (letterFound = false) : '';
+    wordHide.textContent = displayLetter.join(''); // display DOM
+  });
 });
