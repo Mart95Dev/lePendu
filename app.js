a7a8034b505ce8b2d60fe994fcd809e7ed8dc935
Martial Wehrling <d.wehrling@gmail.com>
2021-12-19 21:05:10 +0300

    Ajour fonction lettres choisies et bouton arret / bouton modifier

--- app.js
+++ app.js
@@ -5,8 +5,101 @@ const ctx = canvas.getContext('2d');
 canvas.width = 350;
 canvas.height = 450;
 
-// function potence 1
-const potencePart1 = () => {
+//function level Difficult potence
+const potenceLevelDifficult = () => {
+  ctx.beginPath();
+  ctx.moveTo(30, 400);
+  ctx.lineTo(200, 400);
+  ctx.moveTo(102, 400);
+  ctx.lineTo(102, 50);
+  ctx.moveTo(97, 50);
+  ctx.lineTo(257, 50);
+  ctx.moveTo(102, 100);
+  ctx.lineTo(157, 50);
+  ctx.strokeStyle = 'rgb(124, 80, 8)';
+  ctx.lineWidth = 10;
+  ctx.stroke();
+};
+
+// function rope and head level difficult
+const ropeHeadLevelDifficult = () => {
+  ctx.beginPath();
+  ctx.moveTo(250, 54);
+  ctx.lineTo(250, 110);
+  ctx.strokeStyle = 'rgb(243, 241, 233)';
+  ctx.lineWidth = 7;
+  ctx.stroke();
+  hair();
+  head();
+  eyes();
+  mouth();
+};
+
+// function arm left and arm right
+const armLevelDifficult = () => {
+  ctx.beginPath();
+  ctx.moveTo(250, 165);
+  ctx.lineTo(205, 195);
+  ctx.moveTo(250, 165);
+  ctx.lineTo(295, 195);
+  ctx.strokeStyle = '#e5bca1';
+  ctx.lineWidth = 5;
+  ctx.fill();
+  ctx.stroke();
+};
+
+//function foot left and foot rignt
+const footLevelDifficult = () => {
+  ctx.beginPath();
+  ctx.moveTo(250, 240);
+  ctx.lineTo(295, 283);
+  ctx.moveTo(250, 240);
+  ctx.lineTo(203, 283);
+  ctx.strokeStyle = '#e5bca1';
+  ctx.lineWidth = 5;
+  ctx.fill();
+  ctx.stroke();
+};
+
+// function level easy potence 1
+const potencePartLevelEasy1 = () => {
+  ctx.beginPath();
+  ctx.moveTo(30, 400);
+  ctx.lineTo(200, 400);
+  ctx.strokeStyle = 'rgb(124, 80, 8)';
+  ctx.lineWidth = 10;
+  ctx.stroke();
+};
+// function level easy potence 2
+const potencePartLevelEasy2 = () => {
+  ctx.beginPath();
+  ctx.moveTo(102, 400);
+  ctx.lineTo(102, 50);
+  ctx.strokeStyle = 'rgb(124, 80, 8)';
+  ctx.lineWidth = 10;
+  ctx.stroke();
+};
+// function level easy potence 3
+const potencePartLevelEasy3 = () => {
+  ctx.beginPath();
+  ctx.moveTo(97, 50);
+  ctx.lineTo(257, 50);
+  ctx.strokeStyle = 'rgb(124, 80, 8)';
+  ctx.lineWidth = 10;
+  ctx.stroke();
+};
+// function level easy potence 4
+const potencePartLevelEasy4 = () => {
+  ctx.beginPath();
+  ctx.moveTo(102, 100);
+  ctx.lineTo(157, 50);
+  ctx.strokeStyle = 'rgb(124, 80, 8)';
+  ctx.lineWidth = 10;
+  ctx.stroke();
+};
+
+// function level Medium potence 1
+const potencePartLevelMedium1 = () => {
   ctx.beginPath();
   ctx.moveTo(30, 400);
   ctx.lineTo(200, 400);
@@ -17,8 +110,8 @@ const potencePart1 = () => {
   ctx.stroke();
 };
 
-//function potence 2
-const potencePart2 = () => {
+//function level Medium  potence 2
+const potencePartLevelMedium2 = () => {
   ctx.beginPath();
   ctx.moveTo(97, 50);
   ctx.lineTo(257, 50);
@@ -146,16 +239,36 @@ const completHead = () => {
   mouth();
 };
 
-const penduOneGamer = new Map();
-penduOneGamer.set(1, potencePart1);
-penduOneGamer.set(2, potencePart2);
-penduOneGamer.set(3, rope);
-penduOneGamer.set(4, completHead);
-penduOneGamer.set(5, body);
-penduOneGamer.set(6, leftArm);
-penduOneGamer.set(7, rightArm);
-penduOneGamer.set(8, rightFoot);
-penduOneGamer.set(9, leftFoot);
+const drawPenduLevelEasy = new Map();
+drawPenduLevelEasy.set(1, potencePartLevelEasy1);
+drawPenduLevelEasy.set(2, potencePartLevelEasy2);
+drawPenduLevelEasy.set(3, potencePartLevelEasy3);
+drawPenduLevelEasy.set(4, potencePartLevelEasy4);
+drawPenduLevelEasy.set(5, rope);
+drawPenduLevelEasy.set(6, completHead);
+drawPenduLevelEasy.set(7, body);
+drawPenduLevelEasy.set(8, leftArm);
+drawPenduLevelEasy.set(9, rightArm);
+drawPenduLevelEasy.set(10, rightFoot);
+drawPenduLevelEasy.set(11, leftFoot);
+
+const drawPenduLevelMedium = new Map();
+drawPenduLevelMedium.set(1, potencePartLevelMedium1);
+drawPenduLevelMedium.set(2, potencePartLevelMedium2);
+drawPenduLevelMedium.set(3, rope);
+drawPenduLevelMedium.set(4, completHead);
+drawPenduLevelMedium.set(5, body);
+drawPenduLevelMedium.set(6, leftArm);
+drawPenduLevelMedium.set(7, rightArm);
+drawPenduLevelMedium.set(8, rightFoot);
+drawPenduLevelMedium.set(9, leftFoot);
+
+const drawPenduLevelDifficult = new Map();
+drawPenduLevelDifficult.set(1, potenceLevelDifficult);
+drawPenduLevelDifficult.set(2, ropeHeadLevelDifficult);
+drawPenduLevelDifficult.set(3, body);
+drawPenduLevelDifficult.set(4, armLevelDifficult);
+drawPenduLevelDifficult.set(5, footLevelDifficult);
 
 ////////////// canvas two gamers ///////////////////////
 
@@ -215,6 +328,7 @@ const modalRule = document.querySelector('.modal-rules');
 const modalParameter = document.querySelector('.modal-parameter');
 const buttonParameter = document.getElementById('begin');
 const contentGame = document.querySelector('.modal-game');
+const stopGame = document.querySelector('.modal-stop-game');
 /// AJOUTER MODALES EN FONCTION DU NOMBRE DE JOUEURS/// function two gamers disabled
 
 window.onload = () => {
@@ -243,23 +357,24 @@ const valid = document.querySelector('.btn');
 let temp = [];
 let nameOne = null;
 let nameTwo = null;
-let numberPlayer = null;
+let numberPlayer = 1;
 let wordEndGame = null;
 let difficult = null;
 let error = false;
+let tabWord = 0;
 
 // choice player number
-buttonGamer.forEach((gamer) => {
+/* buttonGamer.forEach((gamer) => {
   gamer.addEventListener('click', (e) => {
     numberPlayer = e.target.id;
-    /* if (numberPlayer == 1) {
+    if (numberPlayer == 1) {
       pseudoTwo.classList.add('boxDisabled');
     } else {
       pseudoTwo.classList.remove('boxDisabled');
-    } */
+    } 
     gamer.setAttribute('checked', '');
   });
-});
+}); */
 
 //choice pseudo player
 pseudoOne.addEventListener('input', (e) => {
@@ -284,6 +399,11 @@ numberWord.addEventListener('input', (e) => {
     numberWord.value = '';
     return;
   }
+  if (number === 0) {
+    alert('Veuillez Saisir un chiffre supérieur à 0');
+    numberWord.value = '';
+    return;
+  }
   temp.push(number);
   wordEndGame = temp.pop();
 });
@@ -298,7 +418,11 @@ levelGame.forEach((level) => {
 
 /// verified input
 const verify = () => {
-  if (numberPlayer === null || difficult === null || wordEndGame === null) {
+  if (
+    /* numberPlayer === null || */ difficult === null ||
+    wordEndGame === null ||
+    nameOne === null
+  ) {
     alert('Veuillez renseigner tous les paramètres');
     error = true;
     return;
@@ -332,6 +456,10 @@ const resetInputs = () => {
   pseudoOne.value = '';
   pseudoTwo.value = '';
   numberWord.value = '';
+  // temporary two gammer disabled
+  numberPlayer = 1;
+  const idGamer = document.getElementById('1');
+  idGamer.checked = true;
   return;
 };
 
@@ -342,7 +470,6 @@ valid.addEventListener('click', (e) => {
     e.preventDefault;
     error = false;
   } else {
-    resetInputs();
     modalParameter.classList.remove('active');
     modalParameter.classList.add('modal-parameter');
     contentGame.classList.add('active');
@@ -352,97 +479,171 @@ valid.addEventListener('click', (e) => {
     const wordGame = document.querySelector('.word-game');
 
     wordGame.textContent = `Choix du nombre mots à trouver : ${wordEndGame}`;
-    pseudo.textContent = `Pseudo : ${nameOne}`;
-    level.textContent = `Niveau de difficulté : ${difficult}`;
-
+    pseudo.textContent = `Pseudo : ${nameOne.toString().toUpperCase()}`;
+    level.textContent = `Niveau de difficulté : ${difficult
+      .toString()
+      .toUpperCase()}`;
     /// AJOUTER MODALES EN FONCTION DU NOMBRE DE JOUEURS !!! AVEC CONDITION PLAYERS
+    if (tabWord === 0) {
+      gameWordPendu();
+      tabWord = 1;
+    } else {
+      return;
+    }
   }
 });
 
 //////////////////////////// GAME //////////////////////////////
 
-const hideWord = document.querySelector('.hideword');
-const buttonAlphabet = document.querySelectorAll('.alphabet button');
-let count = 0;
-let letterFound = false;
-let tempWord = [];
-let wordHide = document.createElement('p');
-let buttonLetter = '';
+const gameWordPendu = () => {
+  const hideWord = document.querySelector('.hideword');
+  const buttonAlphabet = document.querySelectorAll('.alphabet button');
+  let count = 0;
+  let letterFound = false;
+  let tempWord = [];
+  let wordHide = document.createElement('p');
+  wordHide.classList.add('content-hide-word');
+  let buttonLetter = '';
 
-//base of words
-const words = [
-  'physique',
-  'voiture',
-  'argentine',
-  'forestier',
-  'voyage',
-  'australie',
-  'etats-unies',
-  'echelle',
-  'autoroute',
-  'constitution',
-  'histoire',
-  'geographie',
-];
+  //base of words
+  const words = [
+    'physique',
+    'voiture',
+    'argentine',
+    'forestier',
+    'voyage',
+    'australie',
+    'etats-unies',
+    'echelle',
+    'autoroute',
+    'constitution',
+    'histoire',
+    'geographie',
+  ];
 
-// Choice word
-let wordRandomArray = words[Math.floor(Math.random() * words.length)];
-tempWord.push(wordRandomArray.toUpperCase());
-console.log(tempWord);
-let selectWord = wordRandomArray.toString().toUpperCase();
-console.log(selectWord);
+  // Choice word
+  let wordRandomArray = words[Math.floor(Math.random() * words.length)];
+  tempWord.push(wordRandomArray.toUpperCase());
+  console.log(tempWord);
+  let selectWord = wordRandomArray.toString().toUpperCase();
+  console.log(selectWord);
 
-///////////////////////////////////////
-// code new word
-if (tempWord.length > 1) {
-  if (tempWord.includes(selectWord)) {
-    for (let i = 0; i < words.length; i++) {
-      if (words[i] === wordRandomArray) {
-        words.splice(i, 1);
+  ///////////////////////////////////////
+  // code new word
+  if (tempWord.length > 1) {
+    if (tempWord.includes(selectWord)) {
+      for (let i = 0; i < words.length; i++) {
+        if (words[i] === wordRandomArray) {
+          words.splice(i, 1);
+        }
       }
     }
+
+    wordRandomArray = words[Math.floor(Math.random() * words.length)];
+    console.log('word random apres if ' + wordRandomArray);
+    selectWord = wordRandomArray.toString().toUpperCase();
   }
+  //////////////////////////////////////
 
-  wordRandomArray = words[Math.floor(Math.random() * words.length)];
-  console.log('word random apres if ' + wordRandomArray);
-  selectWord = wordRandomArray.toString().toUpperCase();
-}
-//////////////////////////////////////
+  let wordDisplayHide = selectWord.replace(/[A-Z]/g, '_'); //replace letter for "_" via regex
 
-let wordDisplayHide = selectWord.replace(/[A-Z]/g, '_'); //replace letter for "_" via regex
+  //display hide word
+  wordHide.textContent = wordDisplayHide;
+  hideWord.appendChild(wordHide);
 
-//display hide word
-wordHide.textContent = wordDisplayHide;
-hideWord.appendChild(wordHide);
+  let displayLetter = [...wordDisplayHide]; // extend hide word in array
 
-let displayLetter = [...wordDisplayHide]; // extend hide word in array
+  //function seek letter of word
+  const seekLetter = (userLetter) => {
+    return [...selectWord].map((letter, i) => {
+      if (displayLetter[i] !== '_') return displayLetter[i];
+      if (letter === userLetter) {
+        letterFound = true;
+        return userLetter;
+      } else {
+        return '_';
+      }
+    });
+  };
 
-//function seek letter of word
-const seekLetter = (userLetter) => {
-  return [...selectWord].map((letter, i) => {
-    if (displayLetter[i] !== '_') return displayLetter[i];
-    if (letter === userLetter) {
-      letterFound = true;
-      return userLetter;
-    } else {
-      return '_';
-    }
+  //event alphabet button
+  buttonAlphabet.forEach((letters) => {
+    letters.addEventListener('click', (e) => {
+      buttonLetter = e.target.id.toUpperCase();
+      letters.classList.add('boxDisabled');
+
+      /// box seek letters/////////////
+      let boxSeekLetters = document.querySelector('.seek');
+      boxSeekLetters.insertAdjacentHTML(
+        'beforeend',
+        `<button id="${buttonLetter}" class="btn-letter-seek">${buttonLetter}</button>`
+      );
+
+      /////////////////////////////
+      displayLetter = seekLetter(buttonLetter);
+      if (!letterFound) {
+        // if choice letter is not found - draw pendu
+        if (difficult === 'Facile') {
+          ++count;
+          drawPenduLevelEasy.get(count)();
+        } else if (difficult === 'Moyen') {
+          ++count;
+          drawPenduLevelMedium.get(count)();
+        } else {
+          ++count;
+          drawPenduLevelDifficult.get(count)();
+        }
+
+        letterFound = false;
+      }
+      letterFound === true ? (letterFound = false) : '';
+      wordHide.textContent = displayLetter.join(''); // display DOM
+    });
   });
 };
 
-//event alphabet button
-buttonAlphabet.forEach((letters) => {
-  letters.addEventListener('click', (e) => {
-    buttonLetter = e.target.id.toUpperCase();
-    letters.classList.add('boxDisabled');
-    displayLetter = seekLetter(buttonLetter);
-    if (!letterFound) {
-      // if choice letter is not found - draw pendu
-      ++count;
-      penduOneGamer.get(count)();
-      letterFound = false;
-    }
-    letterFound === true ? (letterFound = false) : '';
-    wordHide.textContent = displayLetter.join(''); // display DOM
-  });
+// reset parameters gamer
+const resetParameters = document.querySelector('.reset-parameters');
+resetParameters.addEventListener('click', () => {
+  modalParameter.classList.add('active');
+  modalParameter.classList.remove('modal-parameter');
+  contentGame.classList.remove('active');
+  contentGame.classList.add('modal-game');
+});
+
+// button stop game
+const endGame = document.querySelector('.end');
+endGame.addEventListener('click', () => {
+  contentGame.classList.remove('active');
+  contentGame.classList.add('modal-game');
+  stopGame.classList.add('active');
+  stopGame.classList.remove('modal-stop-game');
+  const warning = document.querySelector('.warning');
+  warning.textContent = 'Voulez-vous quitter le jeu du pendu ?';
+});
+
+const btnGame = document.querySelector('.go-game');
+btnGame.addEventListener('click', () => {
+  contentGame.classList.add('active');
+  contentGame.classList.remove('modal-game');
+  stopGame.classList.remove('active');
+  stopGame.classList.add('modal-stop-game');
+});
+
+const stopG = document.querySelector('.stop');
+stopG.addEventListener('click', () => {
+  temp = [];
+  nameOne = null;
+  nameTwo = null;
+  numberPlayer = 1;
+  wordEndGame = null;
+  difficult = null;
+  error = false;
+  tabWord = 0;
+  resetInputs();
+
+  stopGame.classList.remove('active');
+  stopGame.classList.add('modal-stop-game');
+  modalRule.classList.add('active');
+  modalRule.classList.remove('modal-rules');
 });
---
+++ appTest.js
@@ -0,0 +1,361 @@
+////////////// canvas one gamer ////////////////////
+
+const canvas = document.getElementById('draw1');
+const ctx = canvas.getContext('2d');
+canvas.width = 350;
+canvas.height = 450;
+
+//function level Difficult potence
+const potenceLevelDifficult = () => {
+  ctx.beginPath();
+  ctx.moveTo(30, 400);
+  ctx.lineTo(200, 400);
+  ctx.moveTo(102, 400);
+  ctx.lineTo(102, 50);
+  ctx.moveTo(97, 50);
+  ctx.lineTo(257, 50);
+  ctx.moveTo(102, 100);
+  ctx.lineTo(157, 50);
+  ctx.strokeStyle = 'rgb(124, 80, 8)';
+  ctx.lineWidth = 10;
+  ctx.stroke();
+};
+
+// function rope and head level difficult
+const ropeHeadLevelDifficult = () => {
+  ctx.beginPath();
+  ctx.moveTo(250, 54);
+  ctx.lineTo(250, 110);
+  ctx.strokeStyle = 'rgb(243, 241, 233)';
+  ctx.lineWidth = 7;
+  ctx.stroke();
+  hair();
+  head();
+  eyes();
+  mouth();
+};
+
+// function arm left and arm right
+const armLevelDifficult = () => {
+  ctx.beginPath();
+  ctx.moveTo(250, 165);
+  ctx.lineTo(205, 195);
+  ctx.moveTo(250, 165);
+  ctx.lineTo(295, 195);
+  ctx.strokeStyle = '#e5bca1';
+  ctx.lineWidth = 5;
+  ctx.fill();
+  ctx.stroke();
+};
+
+//function foot left and foot rignt
+const footLevelDifficult = () => {
+  ctx.beginPath();
+  ctx.moveTo(250, 240);
+  ctx.lineTo(295, 283);
+  ctx.moveTo(250, 240);
+  ctx.lineTo(203, 283);
+  ctx.strokeStyle = '#e5bca1';
+  ctx.lineWidth = 5;
+  ctx.fill();
+  ctx.stroke();
+};
+
+// function level easy potence 1
+const potencePartLevelEasy1 = () => {
+  ctx.beginPath();
+  ctx.moveTo(30, 400);
+  ctx.lineTo(200, 400);
+  ctx.strokeStyle = 'rgb(124, 80, 8)';
+  ctx.lineWidth = 10;
+  ctx.stroke();
+};
+// function level easy potence 2
+const potencePartLevelEasy2 = () => {
+  ctx.beginPath();
+  ctx.moveTo(102, 400);
+  ctx.lineTo(102, 50);
+  ctx.strokeStyle = 'rgb(124, 80, 8)';
+  ctx.lineWidth = 10;
+  ctx.stroke();
+};
+// function level easy potence 3
+const potencePartLevelEasy3 = () => {
+  ctx.beginPath();
+  ctx.moveTo(97, 50);
+  ctx.lineTo(257, 50);
+  ctx.strokeStyle = 'rgb(124, 80, 8)';
+  ctx.lineWidth = 10;
+  ctx.stroke();
+};
+// function level easy potence 4
+const potencePartLevelEasy4 = () => {
+  ctx.beginPath();
+  ctx.moveTo(102, 100);
+  ctx.lineTo(157, 50);
+  ctx.strokeStyle = 'rgb(124, 80, 8)';
+  ctx.lineWidth = 10;
+  ctx.stroke();
+};
+
+// function level Medium potence 1
+const potencePartLevelMedium1 = () => {
+  ctx.beginPath();
+  ctx.moveTo(30, 400);
+  ctx.lineTo(200, 400);
+  ctx.moveTo(102, 400);
+  ctx.lineTo(102, 50);
+  ctx.strokeStyle = 'rgb(124, 80, 8)';
+  ctx.lineWidth = 10;
+  ctx.stroke();
+};
+
+//function level Medium  potence 2
+const potencePartLevelMedium2 = () => {
+  ctx.beginPath();
+  ctx.moveTo(97, 50);
+  ctx.lineTo(257, 50);
+  ctx.moveTo(102, 100);
+  ctx.lineTo(157, 50);
+  ctx.strokeStyle = 'rgb(124, 80, 8)';
+  ctx.lineWidth = 10;
+  ctx.stroke();
+};
+
+//function rope
+const rope = () => {
+  ctx.beginPath();
+  ctx.moveTo(250, 54);
+  ctx.lineTo(250, 110);
+  ctx.strokeStyle = 'rgb(243, 241, 233)';
+  ctx.lineWidth = 7;
+  ctx.stroke();
+};
+
+//function head
+const head = () => {
+  ctx.beginPath();
+  ctx.arc(250, 125, 25, 0, 2 * Math.PI);
+  ctx.strokeStyle = '#e5bca1';
+  ctx.fillStyle = '#e5bca1';
+  ctx.fill();
+  ctx.stroke();
+};
+// function hair of head
+const hair = () => {
+  ctx.beginPath();
+  ctx.arc(250, 118, 26, 0, 2 * Math.PI);
+  ctx.strokeStyle = 'black';
+  ctx.fillStyle = 'black';
+  ctx.fill();
+  ctx.stroke();
+};
+//function eyes of head
+const eyes = () => {
+  ctx.beginPath();
+  ctx.moveTo(235, 118);
+  ctx.lineTo(245, 118);
+  ctx.moveTo(257, 118);
+  ctx.lineTo(267, 118);
+  ctx.strokeStyle = 'black';
+  ctx.lineWidth = 1;
+  ctx.fill();
+  ctx.stroke();
+};
+
+//function mouth of head
+const mouth = () => {
+  ctx.beginPath();
+  ctx.moveTo(250, 130);
+  ctx.lineTo(260, 136);
+  ctx.moveTo(250, 130);
+  ctx.lineTo(240, 136);
+  ctx.strokeStyle = 'black';
+  ctx.lineWidth = 1;
+  ctx.fill();
+  ctx.stroke();
+};
+
+//function body
+const body = () => {
+  ctx.beginPath();
+  ctx.moveTo(250, 153);
+  ctx.lineTo(250, 240);
+  ctx.strokeStyle = '#e5bca1';
+  ctx.lineWidth = 5;
+  ctx.fill();
+  ctx.stroke();
+};
+
+//function arm left
+const leftArm = () => {
+  ctx.beginPath();
+  ctx.moveTo(250, 165);
+  ctx.lineTo(205, 195);
+  ctx.strokeStyle = '#e5bca1';
+  ctx.lineWidth = 5;
+  ctx.fill();
+  ctx.stroke();
+};
+
+//function arm right
+const rightArm = () => {
+  ctx.beginPath();
+  ctx.moveTo(250, 165);
+  ctx.lineTo(295, 195);
+  ctx.strokeStyle = '#e5bca1';
+  ctx.lineWidth = 5;
+  ctx.fill();
+  ctx.stroke();
+};
+
+//function foot right
+const rightFoot = () => {
+  ctx.beginPath();
+  ctx.moveTo(250, 240);
+  ctx.lineTo(295, 283);
+  ctx.strokeStyle = '#e5bca1';
+  ctx.lineWidth = 5;
+  ctx.fill();
+  ctx.stroke();
+};
+
+//function foot left
+const leftFoot = () => {
+  ctx.beginPath();
+  ctx.moveTo(250, 240);
+  ctx.lineTo(203, 283);
+  ctx.strokeStyle = '#e5bca1';
+  ctx.lineWidth = 5;
+  ctx.fill();
+  ctx.stroke();
+};
+
+// function head completed
+const completHead = () => {
+  hair();
+  head();
+  eyes();
+  mouth();
+};
+
+const drawPenduLevelEasy = new Map();
+drawPenduLevelEasy.set(1, potencePartLevelEasy1);
+drawPenduLevelEasy.set(2, potencePartLevelEasy2);
+drawPenduLevelEasy.set(3, potencePartLevelEasy3);
+drawPenduLevelEasy.set(4, potencePartLevelEasy4);
+drawPenduLevelEasy.set(5, rope);
+drawPenduLevelEasy.set(6, completHead);
+drawPenduLevelEasy.set(7, body);
+drawPenduLevelEasy.set(8, leftArm);
+drawPenduLevelEasy.set(9, rightArm);
+drawPenduLevelEasy.set(10, rightFoot);
+drawPenduLevelEasy.set(11, leftFoot);
+
+const drawPenduLevelMedium = new Map();
+drawPenduLevelMedium.set(1, potencePartLevelMedium1);
+drawPenduLevelMedium.set(2, potencePartLevelMedium2);
+drawPenduLevelMedium.set(3, rope);
+drawPenduLevelMedium.set(4, completHead);
+drawPenduLevelMedium.set(5, body);
+drawPenduLevelMedium.set(6, leftArm);
+drawPenduLevelMedium.set(7, rightArm);
+drawPenduLevelMedium.set(8, rightFoot);
+drawPenduLevelMedium.set(9, leftFoot);
+
+const drawPenduLevelDifficult = new Map();
+drawPenduLevelDifficult.set(1, potenceLevelDifficult);
+drawPenduLevelDifficult.set(2, ropeHeadLevelDifficult);
+drawPenduLevelDifficult.set(3, body);
+drawPenduLevelDifficult.set(4, armLevelDifficult);
+drawPenduLevelDifficult.set(5, footLevelDifficult);
+
+//////////////////////////// GAME //////////////////////////////
+
+const hideWord = document.querySelector('.hideword');
+const buttonAlphabet = document.querySelectorAll('.alphabet button');
+let count = 0;
+let letterFound = false;
+let tempWord = [];
+let wordHide = document.createElement('p');
+let buttonLetter = '';
+
+//base of words
+const words = [
+  'physique',
+  'voiture',
+  'argentine',
+  'forestier',
+  'voyage',
+  'australie',
+  'etats-unies',
+  'echelle',
+  'autoroute',
+  'constitution',
+  'histoire',
+  'geographie',
+];
+
+// Choice word
+let wordRandomArray = words[Math.floor(Math.random() * words.length)];
+tempWord.push(wordRandomArray.toUpperCase());
+console.log(tempWord);
+let selectWord = wordRandomArray.toString().toUpperCase();
+console.log(selectWord);
+
+///////////////////////////////////////
+// code new word
+if (tempWord.length > 1) {
+  if (tempWord.includes(selectWord)) {
+    for (let i = 0; i < words.length; i++) {
+      if (words[i] === wordRandomArray) {
+        words.splice(i, 1);
+      }
+    }
+  }
+
+  wordRandomArray = words[Math.floor(Math.random() * words.length)];
+  console.log('word random apres if ' + wordRandomArray);
+  selectWord = wordRandomArray.toString().toUpperCase();
+}
+//////////////////////////////////////
+
+let wordDisplayHide = selectWord.replace(/[A-Z]/g, '_'); //replace letter for "_" via regex
+
+//display hide word
+wordHide.textContent = wordDisplayHide;
+hideWord.appendChild(wordHide);
+
+let displayLetter = [...wordDisplayHide]; // extend hide word in array
+
+//function seek letter of word
+const seekLetter = (userLetter) => {
+  return [...selectWord].map((letter, i) => {
+    if (displayLetter[i] !== '_') return displayLetter[i];
+    if (letter === userLetter) {
+      letterFound = true;
+      return userLetter;
+    } else {
+      return '_';
+    }
+  });
+};
+
+//event alphabet button
+buttonAlphabet.forEach((letters) => {
+  letters.addEventListener('click', (e) => {
+    buttonLetter = e.target.id.toUpperCase();
+    letters.classList.add('boxDisabled');
+    displayLetter = seekLetter(buttonLetter);
+    if (!letterFound) {
+      // if choice letter is not found - draw pendu
+      ++count;
+      drawPenduLevelEasy.get(count)();
+      /* drawPenduLevelMedium.get(count)(); */
+      /* drawPenduLevelDifficult.get(count)(); */
+      letterFound = false;
+    }
+    letterFound === true ? (letterFound = false) : '';
+    wordHide.textContent = displayLetter.join(''); // display DOM
+  });
+});
